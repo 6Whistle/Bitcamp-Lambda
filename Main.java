b5fffe70ca806d1493fe2ebd7189e3d8744813b2
6whistle <camjun0506@icloud.com>
2024-03-11 19:24:31 +0900

    2024-03-11 UPDATE main

--- Main.java
+++ Main.java
@@ -13,7 +13,8 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scan = new Scanner(System.in);
     public static void main(String[] args) throws IOException, SQLException {
-        while(Navigation.navigate(new Scanner(System.in)));
+        while(Navigation.navigate(scan));
     }
 }
